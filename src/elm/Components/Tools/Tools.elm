8d0f6de18acca41704d14a613f56bcb28ac9fb6f
Knovour <knovour@gmail.com>
2017-01-18 03:08:00 +0800

    :art: add tools and footer

--- src/elm/Components/Tools/Tools.elm
+++ src/elm/Components/Tools/Tools.elm
@@ -14,11 +14,37 @@ tools =
     group2 = List.map (\num -> month [] [ Month.current num ]) (List.range 7 12)
   in
     div [ class "tools" ]
-      [ div [ class "month-group" ]
-          [ p [ class "heading" ] [ text "月份" ]
-          , div [ class "group" ] group1
-          , div [ class "group" ] group2
+      [ div [ class "view-options" ]
+        [ p [ class "heading" ] [ text "顯示方式" ]
+        , div [ class "opts-group"]
+          [ div [ class "opts" ]
+            [ div [ class "layout grid view active" ] [ i [ class "material-icons" ] [ text "apps" ] ]
+            , div [ class "layout list view" ] [ i [ class "material-icons" ] [ text "menu" ] ]
+            ]
+          , div [ class "opts" ]
+            [ div [ class "group view active" ] [ text "分頁" ]
+            , div [ class "group view" ] [ text "標籤" ]
+            ]
           ]
+        ]
+      , div [ class "month-group" ]
+        [ p [ class "heading" ] [ text "月份" ]
+        , div [ class "group" ] group1
+        , div [ class "group" ] group2
+        ]
+      , footer [ class "footer" ]
+        [ div [ class "copyright" ] [ text "2017 © Knovour" ]
+        , div [ class "links" ]
+          [ a [ href "https://notes.knovour.ninja" ] [ text "Blog" ]
+          , span [ class "middot" ] [ text "·" ]
+          , a [ href "https://github.com/Knovour/food" ] [ text "GitHub" ]
+          , span [ class "middot" ] [ text "·" ]
+          , span []
+            [ text "Made by "
+            , a [ href "http://elm-lang.org" ] [ text "Elm" ]
+            ]
+          ]
+        ]
       ]
   -- Html.form [ class classes ]
   --   [ fieldset [ class "month" ] [ text "搜尋" ]
