22bedce298c42ae70b54e60ae097ca852f558f8a
Knovour <knovour@gmail.com>
2017-03-15 00:32:47 +0800

    :art: refactoring

--- src/elm/Components/Dimmer.elm
+++ src/elm/Components/Dimmer.elm
@@ -3,25 +3,21 @@ import Html            exposing (Html, div, text)
 import Html.Attributes exposing (class, classList)
 import Html.Events     exposing (onClick)
 
-import Architecture.Main   as Main exposing (..)
-import Architecture.Box    exposing (..)
-import Architecture.Action exposing (..)
+import Architecture.Main as Main exposing (..)
+import Architecture.Box  exposing (..)
 
 
 
 dimmer : Main.Model -> Html Main.Msg
 dimmer { box, action, screen } =
-  let isBoxShow = (box.open || (action.toggleSidebar && screen.width <= 976))
-      classes =
+  let classes =
         classList
           [ ("dimmer", True)
-          , ("_show", isBoxShow)
-          , ("_hide", not isBoxShow)
+          , ("_show", box.open)
+          , ("_hide", not box.open)
           ]
       handleClick =
         if box.open
         then Box <| Close
-        else if (action.toggleSidebar && screen.width <= 976)
-        then Action <| ToggleSidebar
         else NoOp
   in div [ classes, onClick handleClick ] []
