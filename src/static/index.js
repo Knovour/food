87037702d1521b9da6e719c1907ff66d2cfa6d1d
Knovour <knovour@gmail.com>
2017-04-03 00:46:36 +0800

    :art: refactor

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,6 @@
 // pull in desired CSS/SASS files
-require( './styles/main.css' );
+require('./styles/main.css');
 
 // inject bundled Elm app into div#main
-const Elm = require( '../elm/Main' );
+const Elm = require('../elm/Main');
 Elm.Main.embed(document.getElementById('app'));
